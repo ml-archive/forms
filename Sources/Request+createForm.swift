d2871581ef04b942f96dfc24608bce72e3e70fdb
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2017-08-01 12:44:28 +0200

    Support formURLEncoded data as well as JSON

--- Sources/Request+createForm.swift
+++ Sources/Request+createForm.swift
@@ -2,7 +2,7 @@ import Vapor
 
 extension Request {
     public func createForm<T: Form>() throws -> T where T: JSONInitializable {
-        guard let json = self.json else {
+        guard let json = self.json ?? formURLEncoded.map(JSON.init) else {
             throw Abort.badRequest
         }
         return try T.init(json: json)
